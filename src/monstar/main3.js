d6e5128c821396d46ebc199fb1f622c10c4cbe75
tungdd <tung.dang@monstar-lab.com>
2021-01-18 17:52:51 +0700

    update code

---
+++ src/monstar/main3.js
@@ -0,0 +1,14 @@
+(function () {
+    var body = document.body,
+        company = 'Monstarlab Lifetime',
+        colors = {
+            main: "#ffff00",
+            text: "#53575a",
+        },
+        link = 'https://monstar-lab.com/global';
+    
+    var nodeModal = document.createElement('div');
+    nodeModal.innerHTML = `<div style="font-size: 40px">haha</div>`;
+    
+    body.appendChild(nodeModal);
+})();
