c2d75c0486d95940d8090298376cdd0dd98f93ec
tungdd <tung.dang@monstar-lab.com>
2021-01-19 08:45:17 +0700

    fix

--- src/monstar/main3.js
+++ src/monstar/main3.js
@@ -52,161 +52,133 @@
         modalLink: modal.querySelector("#modal-link"),
     };
 
-    var useStyles = function () {
-        return {
-            flexCenter: {
-                display: "flex",
-                "justify-content": "center",
-                "align-items": "center",
-                "flex-wrap": "wrap",
-            },
-            fontMain: {
-                "font-family": "Helvetica, sans-serif",
-            },
-            textLink: {
-                color: configs.colors.text,
-                "text-decoration": "underline",
-            },
-            modal: function () {
-                return {
-                    "box-sizing": "border-box",
-                    position: "fixed",
-                    top: 0,
-                    bottom: 0,
-                    right: 0,
-                    left: 0,
-                    background: "rgba(0, 0, 0, 0.5)",
-                    "line-height": 1.5,
-                    transition: "all 0.5s ease",
-                    "z-index": 9999,
-                    opacity: 0,
-                    visibility: "hidden",
-                    ...this.flexCenter,
-                    ...this.fontMain,
-                };
-            },
-            modalContent: function () {
-                return {
-                    position: "absolute",
-                    padding: "10px",
-                    width: "95%",
-                    "max-width": "480px",
-                    "min-height": "480px",
-                    background: configs.colors.main,
-                    "text-align": "center",
-                    "box-shadow": "0px 2px 25px rgba(0, 0, 0, 0.25)",
-                    ...this.flexCenter,
-                };
-            },
-            modalClose: function () {
-                return {
-                    position: "absolute",
-                    right: "10px",
-                    top: "10px",
-                    "font-size": "28px",
-                    color: configs.colors.text,
-                    cursor: "pointer",
-                    "line-height": 0.8,
-                };
-            },
-            modalSubtitle: function () {
-                return {
-                    "font-size": "32px",
-                };
-            },
-            modalTitle: function () {
-                return {
-                    margin: "20px 0 50px 0",
-                    "font-size": "40px",
-                    "font-weight": "bold",
-                    ...this.flexCenter,
-                };
-            },
-            modalIcons: function () {
-                return {
-                    position: "relative",
-                    width: "27px",
-                    height: "27px",
-                    "margin-left": "20px",
-                };
-            },
-            icon: {
-                width: "6px",
-                height: "6px",
-                background: "#000",
-                position: "absolute",
-            },
-            icon1: function () {
-                return {
-                    top: 0,
-                    left: 0,
-                    ...this.icon,
-                };
-            },
-            icon2: function () {
-                return {
-                    bottom: 0,
-                    left: 0,
-                    ...this.icon,
-                };
-            },
-            icon3: function () {
-                return {
-                    top: 0,
-                    right: 0,
-                    ...this.icon,
-                };
-            },
-            icon4: function () {
-                return {
-                    bottom: 0,
-                    right: 0,
-                    ...this.icon,
-                };
-            },
-            modalSummary: function () {
-                return {
-                    padding: "0 50px",
-                    "margin-bottom": "20px",
-                    color: configs.colors.text,
-                    "font-size": "18px",
-                };
-            },
-            modalLink: function () {
-                return {
-                    "font-weight": "bold",
-                    "font-size": "24px",
-                    ...this.textLink,
-                };
-            },
-            topHeader: function () {
-                return {
-                    ...this.flexCenter,
-                    ...this.fontMain,
-                    position: "relative",
-                    "min-height": "50px",
-                    padding: "10px",
-                    "text-align": "center",
-                    "border-bottom": "1px solid #EFF0F0",
-                    "border-top": `3px solid ${configs.colors.main}`,
-                    "background-color": "#fff",
-                    color: configs.colors.text,
-                    "font-size": "14px",
-                    "line-height": 1.5,
-                    "z-index": 9999,
-                };
-            },
-            linkHeader: function () {
-                return {
-                    ...this.textLink,
-                };
-            },
-        };
+    var baseStyles = {
+        flexCenter: {
+            display: "flex",
+            "justify-content": "center",
+            "align-items": "center",
+            "flex-wrap": "wrap",
+        },
+        fontMain: {
+            "font-family": "Helvetica, sans-serif",
+        },
+        textLink: {
+            color: configs.colors.text,
+            "text-decoration": "underline",
+        },
+        icon: {
+            width: "6px",
+            height: "6px",
+            background: "#000",
+            position: "absolute",
+        },
     };
-    var classes = useStyles();
+
+    var useStyles = {
+        modal: {
+            "box-sizing": "border-box",
+            position: "fixed",
+            top: 0,
+            bottom: 0,
+            right: 0,
+            left: 0,
+            background: "rgba(0, 0, 0, 0.5)",
+            "line-height": 1.5,
+            transition: "all 0.5s ease",
+            "z-index": 9999,
+            opacity: 0,
+            visibility: "hidden",
+            ...baseStyles.flexCenter,
+            ...baseStyles.fontMain,
+        },
+        modalContent: {
+            position: "absolute",
+            padding: "10px",
+            width: "95%",
+            "max-width": "480px",
+            "min-height": "480px",
+            background: configs.colors.main,
+            "text-align": "center",
+            "box-shadow": "0px 2px 25px rgba(0, 0, 0, 0.25)",
+            ...baseStyles.flexCenter,
+        },
+        modalClose: {
+            position: "absolute",
+            right: "10px",
+            top: "10px",
+            "font-size": "28px",
+            color: configs.colors.text,
+            cursor: "pointer",
+            "line-height": 0.8,
+        },
+        modalSubtitle: {
+            "font-size": "32px",
+        },
+        modalTitle: {
+            margin: "20px 0 50px 0",
+            "font-size": "40px",
+            "font-weight": "bold",
+            ...baseStyles.flexCenter,
+        },
+        modalIcons: {
+            position: "relative",
+            width: "27px",
+            height: "27px",
+            "margin-left": "20px",
+        },
+        icon1: {
+            top: 0,
+            left: 0,
+            ...baseStyles.icon,
+        },
+        icon2: {
+            bottom: 0,
+            left: 0,
+            ...baseStyles.icon,
+        },
+        icon3: {
+            top: 0,
+            right: 0,
+            ...baseStyles.icon,
+        },
+        icon4: {
+            bottom: 0,
+            right: 0,
+            ...baseStyles.icon,
+        },
+        modalSummary: {
+            padding: "0 50px",
+            "margin-bottom": "20px",
+            color: configs.colors.text,
+            "font-size": "18px",
+        },
+        modalLink: {
+            "font-weight": "bold",
+            "font-size": "24px",
+            ...baseStyles.textLink,
+        },
+        topHeader: {
+            ...baseStyles.flexCenter,
+            ...baseStyles.fontMain,
+            position: "relative",
+            "min-height": "50px",
+            padding: "10px",
+            "text-align": "center",
+            "border-bottom": "1px solid #EFF0F0",
+            "border-top": `3px solid ${configs.colors.main}`,
+            "background-color": "#fff",
+            color: configs.colors.text,
+            "font-size": "14px",
+            "line-height": 1.5,
+            "z-index": 9999,
+        },
+        linkHeader: {
+            ...baseStyles.textLink,
+        },
+    };
 
     Object.keys(nodesModal).forEach((elm) => {
-        Object.entries(classes[elm]()).forEach(
+        Object.entries(useStyles[elm]).forEach(
             ([key, value]) => (nodesModal[elm].style[key] = value)
         );
     });
@@ -220,7 +192,7 @@
             linkHeader: topHeader.querySelector("a"),
         };
         Object.keys(nodesTopHeader).forEach((elm) => {
-            Object.entries(classes[elm]()).forEach(
+            Object.entries(useStyles[elm]).forEach(
                 ([key, value]) => (nodesTopHeader[elm].style[key] = value)
             );
         });
